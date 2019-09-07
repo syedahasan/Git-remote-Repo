33f2b704ddc5bf11a45d3b83f51b5f256a585f7c
Syeda Nasuha Hasan <syedahasan@syedas-mbp.lan>
2019-09-07 13:40:12 -0400

    first commit

---
+++ Testng/src/test/java/com/bit/test/RegrassionTest.java
@@ -0,0 +1,27 @@
+package com.bit.test;
+
+import org.testng.annotations.Test;
+
+public class RegrassionTest extends BaseTest{
+
+	
+	@Test
+	public void test2 () {
+	System.out.println("Hi");	
+		
+	}
+	
+	@Test
+	public void test4 () {
+		System.out.println("Hello");	
+			
+		}
+	
+	@Test
+	public void test5 () {
+		System.out.println("Hello world");	
+			
+		}
+	
+	
+}
---
+++ Testng/src/test/java/com/bit/test/SmokeTest.java
@@ -0,0 +1,37 @@
+package com.bit.test;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest extends BaseTest {
+
+	@Test
+	public void test1 () {
+	System.out.println("test1");	
+			
+	}
+	
+	@Test
+	public void test2 () {
+	System.out.println("test2");	
+	}
+    
+	@Test
+	public void test3 () {
+	System.out.println("test3");	
+	}
+	
+	//Why we name testng.xml? and when we use it?
+	//What is design and sourse in testng.xml?
+	//What is @Suite and @Group?
+	//When we execute @Suite annotation?
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
